ca8d847da95fb0a692cb2d536ed8457e838d41e4
Koyeb App <koyebapp@gmail.com>
2025-01-31 11:42:06 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
-const VideoDownloader = require('./photooxy');
+const VideoLinkExtractor = require('./VideoLinkExtractor'); // Import the new class
 
-const downloader = new VideoDownloader();
+const extractor = new VideoLinkExtractor();
 
 (async () => {
   try {
-    await downloader.download(
+    // Use the getDownloadLink function to get the download link
+    const downloadLink = await extractor.getDownloadLink(
       'https://youtube.com/shorts/LiyfWStYQRs?si=DsjuflFZ901pgJpi'
+    );
+    console.log('Download Link:', downloadLink); // Log the download link
   } catch (error) {
-    console.error(error.message);
+    console.error('Error:', error.message); // Catch and log any errors
   }
 })();
