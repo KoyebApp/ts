7f9ada3059dc1719eecd662b151c869566ae5760
Koyeb App <koyebapp@gmail.com>
2025-01-31 11:16:36 +0500

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const downloader = new VideoDownloader();
 (async () => {
   try {
     await downloader.download(
-      'https://youtube.com/shorts/LiyfWStYQRs?si=DsjuflFZ901pgJpi', // Replace with the YouTube video URL
+      'https://youtube.com/shorts/LiyfWStYQRs?si=DsjuflFZ901pgJpi' // Replace with the YouTube video URL
   } catch (error) {
     console.error(error.message);
   }
