6e45ab7c7b36aff055ed33402b64114e48fedf9f
Koyeb App <koyebapp@gmail.com>
2025-01-31 12:43:35 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
-const VideoLinkExtractor = require('./VideoLinkExtractor');
-
-const extractor = new VideoLinkExtractor();
+const VideoDownloader = require('./VideoDownloader');
+const downloader = new VideoDownloader();
 
 (async () => {
   try {
-    const youtubeUrl = 'https://youtube.com/shorts/LiyfWStYQRs?si=TPx_aYFePMT-8QvE';
-    const downloadLink = await extractor.getDownloadLink(youtubeUrl);
-    console.log('Download Link:', downloadLink); // Log the final download link
+    await downloader.download('https://youtube.com/shorts/LiyfWStYQRs?si=DsjuflFZ901pgJpi', 'output_video.mp4');
   } catch (error) {
-    console.error('Error:', error.message); // Catch and log any errors
+    console.error(error.message);
   }
 })();
